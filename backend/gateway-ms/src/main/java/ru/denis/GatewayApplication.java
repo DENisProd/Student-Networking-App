0da26c1249af21bdf7d829ee22ae3a185e0ae758
Denis <guly.denis2014@yandex.ru>
2025-01-07 01:15:02 +0300

    fix

--- backend/gateway-ms/src/main/java/ru/denis/GatewayApplication.java
+++ backend/gateway-ms/src/main/java/ru/denis/GatewayApplication.java
@@ -2,6 +2,7 @@ package ru.denis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.denis.configuration.EnvInitializer;
 
 @SpringBootApplication
 public class GatewayApplication
